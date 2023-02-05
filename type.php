0944a1e9ed39577e1d7c4db1c900f9cfbf09ecec
Lucas Paiva <86642899+LucasMaPaiva@users.noreply.github.com>
2023-02-05 15:37:21 -0400

    add(type):testing variables

---
+++ type.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="_css/style.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+<div>
+    <?php
+    // testing variables
+    $n = 4;
+    $na = "Lucas <br>";
+    echo $na;
+
+    //testing concatenation
+    $age = 21;
+    $name = "Lauren";
+    //echo $name. " is ". $age. " old years";
+    echo "$name is $age old years";
+    ?>
+</div>
+</body>
+</html>
